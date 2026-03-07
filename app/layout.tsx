ee49f156043ba7250cefa9b59e05628141b16987
Ruslan <05rustik04@gmail.com>
2026-03-07 13:06:51 +0100

    Create NextJs App

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Месьє Круасан",
+  description: "Випічка на живій заквасці",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
