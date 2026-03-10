4ce7d61da49893561732acaede28c3d3874c8fcf
Ruslan <05rustik04@gmail.com>
2026-03-10 16:05:23 +0100

    add GraphQL configuration

---
+++ graphql.config.ts
@@ -0,0 +1,20 @@
+import type { IGraphQLConfig } from 'graphql-config'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.local' })
+
+const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN
+const token = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN
+
+const config: IGraphQLConfig = {
+  schema: {
+    [`https://${domain}/api/2026-01/graphql.json`]: {
+      headers: {
+        'X-Shopify-Storefront-Access-Token': token!,
+      },
+    },
+  },
+  documents: 'graphql/**/*.graphql',
+}
+
+export default config
