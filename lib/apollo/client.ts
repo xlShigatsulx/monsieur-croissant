343fd060c217042f7092f3a780b3b62be359e1c9
Ruslan <05rustik04@gmail.com>
2026-03-10 16:31:36 +0100

    Add Apollo Client setup for GraphQL communication with Shopify

---
+++ lib/apollo/client.ts
@@ -0,0 +1,30 @@
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { SetContextLink } from '@apollo/client/link/context'
+import { SHOPIFY_CONFIG, STOREFRONT_API_URL } from '@/lib/shopify/config'
+
+function makeClient() {
+  const httpLink = new HttpLink({ uri: STOREFRONT_API_URL })
+
+  const authLink = new SetContextLink((prevContext, _) => ({
+    headers: {
+      ...prevContext.headers,
+      'X-Shopify-Storefront-Access-Token': SHOPIFY_CONFIG.storefrontAccessToken,
+    },
+  }))
+
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  })
+}
+
+let browserClient: ApolloClient | null = null
+
+export function getClient() {
+  if (typeof window === 'undefined') {
+    return makeClient()
+  }
+
+  if (!browserClient) browserClient = makeClient()
+  return browserClient
+}
