1407c629463b8baad37d4dbfc00b8d8377aac958
Ruslan <05rustik04@gmail.com>
2026-03-10 16:05:05 +0100

    Add GraphQL codegen configuration

---
+++ codegen.ts
@@ -0,0 +1,40 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.local' })
+
+const domain = process.env.NEXT_PUBLIC_SHOPIFY_STORE_DOMAIN
+const token = process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_TOKEN
+
+const config: CodegenConfig = {
+  schema: {
+    [`https://${domain}/api/2025-04/graphql.json`]: {
+      headers: {
+        'X-Shopify-Storefront-Access-Token': token!,
+      },
+    },
+  },
+  documents: 'graphql/**/*.graphql',
+  generates: {
+    'graphql/generated/graphql.ts': {
+      plugins: [
+        'typescript',
+        'typescript-operations',
+        'typescript-react-apollo',
+      ],
+      config: {
+        withHooks: true,
+        withComponent: false,
+        withHOC: false,
+        scalars: {
+          Decimal: 'string',
+          URL: 'string',
+          DateTime: 'string',
+          HTML: 'string',
+        },
+      },
+    },
+  },
+}
+
+export default config
