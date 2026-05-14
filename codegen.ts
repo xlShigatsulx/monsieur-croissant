68ce361d856d5d64bb89eaa24fa4240c1c7ed942
Ruslan <05rustik04@gmail.com>
2026-05-14 23:11:58 +0200

    fix: apollo import path in codegen config

--- codegen.ts
+++ codegen.ts
@@ -26,8 +26,8 @@ const config: CodegenConfig = {
         withHooks: true,
         withComponent: false,
         withHOC: false,
-        apolloReactHooksImportFrom: '@apollo/client/react',
-        apolloReactCommonImportFrom: '@apollo/client/react',
+        apolloReactHooksImportFrom: '@apollo/client',
+        apolloReactCommonImportFrom: '@apollo/client',
         scalars: {
           Decimal: 'string',
           URL: 'string',
