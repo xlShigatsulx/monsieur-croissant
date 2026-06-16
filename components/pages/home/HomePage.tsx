df64a088c78b17f1b3fadeaa6ac00e6fb2401038
Ruslan <05rustik04@gmail.com>
2026-06-16 12:38:16 +0200

    fix: adjust layout structure in HomePage for improved alignment

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -2,7 +2,7 @@ import ProductSlider from '@/components/slider/ProductSlider'
 
 export default function HomePage() {
   return (
-    <div className='min-h-screen flex flex-col justify-center px-4 py-10'>
+    <div className='min-h-screen flex justify-center px-4 py-10'>
       <ProductSlider />
     </div>
   )
