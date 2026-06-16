90433feb6bf2df81417f97daa910776726ee376b
Ruslan <05rustik04@gmail.com>
2026-06-16 12:21:11 +0200

    feat: integrate next-intl plugin for internationalization support

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
 import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
+
+const withNextIntl = createNextIntlPlugin('./i18n/request.ts')
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -6,10 +9,6 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
       {
         protocol: 'https',
         hostname: '*.myshopify.com',
@@ -22,4 +21,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default nextConfig
+export default withNextIntl(nextConfig)
