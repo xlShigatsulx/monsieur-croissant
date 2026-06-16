19325313865c422fd76c69151c57cbd76fd1acc5
Ruslan <05rustik04@gmail.com>
2026-06-16 12:21:26 +0200

    feat: add middleware for internationalization support with locale configuration

---
+++ proxy.ts
@@ -0,0 +1,11 @@
+import createMiddleware from 'next-intl/middleware'
+
+export default createMiddleware({
+  locales: ['uk', 'en'],
+  defaultLocale: 'uk',
+  localePrefix: 'as-needed',
+})
+
+export const config = {
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
+}
