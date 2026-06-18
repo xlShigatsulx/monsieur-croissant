5d9d26b02cfe93bdf3a3fadad8f865f4ceadd65d
Ruslan <05rustik04@gmail.com>
2026-06-18 19:52:38 +0200

    feat: import SearchInput component and adjust layout in HomePage for better alignment

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,8 +1,9 @@
 import ProductSlider from '@/components/slider/ProductSlider'
+import { SearchInput } from '@/components/ui/Search/SearchInput'
 
 export default function HomePage() {
   return (
-    <div className='min-h-screen flex justify-center px-4 py-10'>
+    <div className='min-h-screen flex flex-col items-center px-4 py-10'>
       <ProductSlider />
     </div>
   )
