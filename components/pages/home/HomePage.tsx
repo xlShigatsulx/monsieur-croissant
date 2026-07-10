a0d7e5e8df9a7c96dfdc76e5d9f0df5d9fb13c7c
Ruslan <05rustik04@gmail.com>
2026-07-10 14:28:50 +0200

    feat: restructure HomePage layout for improved component organization

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,10 +1,25 @@
-import ProductSlider from '@/components/slider/ProductSlider'
-import { SearchInput } from '@/components/ui/Search/SearchInput'
+import ProductSlider from '@/components/slider/ProductSlider';
+import { ContactTeaser } from './ContactTeaser';
+import { AboutTeaser } from './AboutTeaser';
+import { BestsellersSection } from './BestsellersSection';
+import { CategoriesSection } from './CategoriesSection';
+import { HeroSection } from './HeroSection/HeroSection';
+import { USPStrip } from './USPStrip';
 
 export default function HomePage() {
   return (
-    <div className='min-h-screen flex flex-col items-center px-4 py-10'>
-      <ProductSlider />
+    <div className="min-h-screen">
+      <HeroSection />
+
+      <div className="flex flex-col items-center px-4">
+        <ProductSlider />
+      </div>
+
+      <USPStrip />
+      <CategoriesSection />
+      <BestsellersSection />
+      <AboutTeaser />
+      <ContactTeaser />
     </div>
-  )
+  );
 }
